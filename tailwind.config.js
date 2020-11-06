722778173c888af04339fbfc7340c847cc656305
frank.gairal <frank@autofi.io>
2020-11-05 23:26:53 -0800

    fix(css): details

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,7 @@ module.exports = {
   },
   purge: false,
   theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        black: '#000',
-        white: '#fff',
-        gray: {
-          100: '#f7fafc',
-          900: '#1a202c',
-        },
-      },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [],
