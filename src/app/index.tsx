5c72952465bc5054f15c9f4f580841d5ab1f019a
frank.gairal <frank@autofi.io>
2020-11-06 01:04:27 -0800

    fix(build): add prod build

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react';
 
-// import { Title } from './components/Title';
+import { Title } from './components/Title';
 
-export const App: FC = () => <div>{/* <Title>React Seed</Title> */}</div>;
+export const App: FC = () => (
+  <div>
+    <Title>React Seed</Title>
+  </div>
+);
