8ff1859b740ef5d8a869918bafbc34acc7e06d1e
frank.gairal <frank@autofi.io>
2020-11-28 17:32:04 -0800

    fix: base structure

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,8 +1,5 @@
 import React, { FC } from 'react';
 
 export const Title: FC = ({ children }) => (
-  <>
-    <h1 className="text-3xl p-2">{children}</h1>
-    <hr className="border" />
-  </>
+  <h1 className="text-3xl p-2">{children}</h1>
 );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,11 @@ import React, { FC } from 'react';
 
 import { Title } from './components/Title';
 
-export const App: FC = () => (
-  <>
-    <Title>React Seed</Title>
-  </>
-);
+export const App: FC = () => {
+  return (
+    <>
+      <Title>React Seed</Title>
+      <hr className="border" />
+    </>
+  );
+};
