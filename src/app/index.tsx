35f8bf2e93538b0d2b21a01cd50dc68cff41ea30
frank.gairal <frank@autofi.io>
2020-11-28 16:15:15 -0800

    fix: html dom

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,8 +7,8 @@ export const App: FC = () => {
   // const { state, changeTitle } = useApp();
 
   return (
-    <main className="p-4">
+    <>
       <Title>React Seed</Title>
-    </main>
+    </>
   );
 };
