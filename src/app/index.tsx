ce658ea870ee9c5dd33c5d0d439f9d03571f2ed3
frank.gairal <frank@autofi.io>
2021-06-06 14:29:55 -0700

    feat(trello): test

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,24 @@
 import React, { FC } from 'react';
+import { Column } from './Column';
 
 import { Title } from './Title';
+import { useTrello } from './useTrello';
+import { TrelloContext } from './context';
+import './index.css';
 
 export const App: FC = () => {
+  const [data, context] = useTrello();
+
   return (
-    <>
-      <Title>React Seed</Title>
-      <hr className="border" />
-    </>
+    <TrelloContext.Provider value={context}>
+      <header>
+        <Title>React Seed</Title>
+      </header>
+      <main className="main">
+        {data.map((col) => (
+          <Column {...col} />
+        ))}
+      </main>
+    </TrelloContext.Provider>
   );
 };
