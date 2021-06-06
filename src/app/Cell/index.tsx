ff64e40a2bd874a8fd083e1044047b91f80552a7
frank.gairal <frank@autofi.io>
2021-06-06 16:06:06 -0700

    fix: merge upstream

--- src/app/Cell/index.tsx
+++ src/app/Cell/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useAppContext } from '../AppContextProvider/context';
+import { useAppContext } from '../AppContext';
 import { Cel } from '../types';
 
 import './Cell.scss';
