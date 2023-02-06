a32648eaeff2d6529e6abbe329dd7550fdab5105
frank.gairal <frank.gairal@swile.co>
2023-02-06 09:34:03 +0100

    fix: use swc instead of babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // eslint-disable-next-line import/no-default-export
-export default defineConfig(() => ({
-  plugins: [react()],
-}));
+export default defineConfig({ plugins: [react()] });
